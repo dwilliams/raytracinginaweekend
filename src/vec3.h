02d9fe868a0566285a66798c2562db9894bea39b
Daniel Williams <dwilliams@port8080.net>
2025-09-25 04:46:54 -0700

    Found the one char bug.  Makes the dot product math all wrong.

--- src/vec3.h
+++ src/vec3.h
@@ -87,7 +87,7 @@ inline Vec3 operator/(const Vec3& v, double t) {
 }
 
 inline double dot(const Vec3& u, const Vec3& v) {
-    return (u.e[0] * v.e[0]) + (u.e[1] * v.e[1]) + (u.e[2] * u.e[2]);
+    return (u.e[0] * v.e[0]) + (u.e[1] * v.e[1]) + (u.e[2] * v.e[2]);
 }
 
 inline Vec3 cross(const Vec3& u, const Vec3& v) {
