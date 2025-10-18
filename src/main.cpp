8d2180d3b351199b37bfd47de6b961c8de8bb409
Daniel Williams <dwilliams@port8080.net>
2025-10-18 13:30:59 -0600

    Chapter 11 done.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "rtweekend.h"
 
 #include "camera.h"
+#include "dielectric.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "lambertian.h"
@@ -26,12 +27,14 @@ int main(void) {
 
     std::shared_ptr<Material> material_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
     std::shared_ptr<Material> material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-    std::shared_ptr<Material> material_left = std::make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.3);
+    std::shared_ptr<Material> material_left = std::make_shared<Dielectric>(1.5);
+    std::shared_ptr<Material> material_bubble = std::make_shared<Dielectric>(1.0 / 1.5);
     std::shared_ptr<Material> material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.01);
 
     world.add(std::make_shared<Sphere>(Point3(0.0, -100.5, -1), 100.0, material_ground));
     world.add(std::make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, material_center));
     world.add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
     world.add(std::make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
@@ -39,6 +42,7 @@ int main(void) {
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 256;
+    //cam.image_width = 1920;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
